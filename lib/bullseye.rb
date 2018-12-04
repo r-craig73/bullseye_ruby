2b78c439d1bb2747cb265ee4294925b4c2359ce4
Ron Craig <ron.craig@comcast.net>
2018-12-04 00:58:16 -0800

    add ruby files

---
+++ lib/bullseye.rb
@@ -0,0 +1,13 @@
+require('pry')
+
+# Board class, initializing arguments and bullseye method
+class Board
+  def initialize(targets, positions, closest_targets)
+    @targets = targets
+    @positions = positions
+    @closest_targets = closest_targets
+  end
+
+  def bullseye
+  end
+end
---
+++ spec/bullseye_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('bullseye')
+
+describe('Board') do
+  describe('bullseye') do
+    it('returns the closest dart position to the bullseye') do
+      board = Board.new(3, [[3, 4], [2, -2], [1, 2]], 1)
+      expect(board.bullseye).to(eq([1, 2]))
+    end
+  end
+end
