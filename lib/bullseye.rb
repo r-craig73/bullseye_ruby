6f9b92c9bb796f0cfeb6d183923693d5a32b977d
Ron Craig <ron.craig@comcast.net>
2018-12-05 23:57:24 -0800

    pass first spec :tada:

--- lib/bullseye.rb
+++ lib/bullseye.rb
@@ -9,15 +9,47 @@ class Board
   end
 
   def bullseye
-    # darts = @targets
+    darts = @targets
     locations = @positions
-    # near_bullseye = @closest_targets
+    near_bullseye = @closest_targets
+
+    # add new_array array to square each location element [x^2]
     new_array = []
-    locations.each do |location|
-      # add nested loops to calculate the location radius
-      new_array.push(location * 2)
+    i = 0
+    while i < darts
+      j = 0
+      while j < 2
+        z = locations[i][j]**2
+        new_array.push(z)
+        j += 1
+      end
+      i += 1
     end
-    new_array
-    # Math.sqrt(locations[2][0]**2 + locations[2][1]**2)
+
+    # add radius array to calculate x & y distance [radius]
+    radius = []
+    k = 0
+    while k < darts
+      w = new_array[k * 2]
+      x = new_array[k * 2 + 1]
+      distance = Math.sqrt(w + x)
+      radius.push(distance)
+
+      # append radius to each location [x, y, radius]
+      locations[k] << radius[k]
+      k += 1
+    end
+
+    # spots = []
+    p = 0
+    while p < near_bullseye
+      # sort each location by ascending radius [x, y, radius]
+      locations.sort_by!(&:last)
+      close_targets = locations[p]
+      close_targets.pop
+      close_targets.push
+      p += 1
+    end
+    close_targets
   end
 end
