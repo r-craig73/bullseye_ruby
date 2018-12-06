993eefb1762b4fc918c45c6660f58e77cf9c3d32
Ron Craig <ron.craig@comcast.net>
2018-12-06 00:52:56 -0800

    add second spec to include a nested array for multiple targets

--- spec/bullseye_spec.rb
+++ spec/bullseye_spec.rb
@@ -7,5 +7,10 @@ describe('Board') do
       board = Board.new(3, [[3, 4], [2, -2], [1, 2]], 1)
       expect(board.bullseye).to(eq([1, 2]))
     end
+
+    it('returns the closest dart position to the bullseye') do
+      board = Board.new(3, [[3, 4], [2, -2], [1, 2]], 2)
+      expect(board.bullseye).to(eq([[1, 2], [2, -2]]))
+    end
   end
 end
